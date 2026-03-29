ce80d64228004b255ff3af22e309f17c1e6cac2e
srgkiryanov84 <srgkiryanov@yandex.ru>
2026-03-29 20:42:39 +0300

    Перезапуск служб Linux

---
+++ RestartServices/RestartServices.ps1
@@ -0,0 +1,15 @@
+﻿cls
+$serversname = @('your.server1.IP', 'your.server2.IP')
+$username = 'root'
+$password = 'YourRootPassword'
+$plinkpath = 'C:\Distr\PuTTY\'
+foreach ($servername in $serversname){
+    $commandRestartSSSD = 'systemctl restart sssd'
+    #$commandRestartXRDP = 'systemctl restart xrdp'
+    #$commandRestartZabbix = 'systemctl restart zabbix-agent'
+    $commandoutput = echo y | &($plinkpath + "plink.exe") -pw $password $username@$servername $commandRestartSSSD
+    #$commandoutput = echo y | &($plinkpath + "plink.exe") -pw $password $username@$servername $commandRestartXRDP
+    #$commandoutput = echo y | &($plinkpath + "plink.exe") -pw $password $username@$servername $commandRestartZabbix
+    Write-Host "Перезапущен sssd на сервере " $servername
+    Write-Host "==============================="
+}
